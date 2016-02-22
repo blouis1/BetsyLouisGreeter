36dac310d3a6a9fa624865216aa8c7b52f213307
Betsy Louis <blouis1@my.westga.edu>
2016-02-22 16:36:19 -0500

    Reverse button is disabled when the activity starts.

--- app/src/androidTest/java/edu/westga/betsylouisgreeter/MainActivityTests.java
+++ app/src/androidTest/java/edu/westga/betsylouisgreeter/MainActivityTests.java
@@ -10,37 +10,48 @@ import android.widget.TextView;
  * Created by Betsy on 2/22/2016.
  */
 public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity activity;
+
     public MainActivityTests() {
         super(MainActivity.class);
     }
 
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        this.activity = getActivity();
+    }
+
     public void testActivityExists() {
-        MainActivity activity = getActivity();
-        assertNotNull(activity);
+        assertNotNull(this.activity);
+    }
+
+    public void testReverseButtonIsDisabled() {
+        Button reverseButton =
+                (Button) this.activity.findViewById(R.id.reverse_button);
+        assertTrue(!reverseButton.isEnabled());
     }
 
     public void testGreet() {
-        MainActivity activity = getActivity();
-
         final EditText nameEditText =
-                (EditText) activity.findViewById(R.id.greet_edit_text);
+                (EditText) this.activity.findViewById(R.id.greet_edit_text);
 
         getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
-                nameEditText.requestFocus();
-                //nameEditText.setText("Jake");
+                //nameEditText.requestFocus();
+                nameEditText.setText("Jake");
             }
         });
 
-        getInstrumentation().waitForIdleSync();
-        getInstrumentation().sendStringSync("Jake");
+        /*getInstrumentation().waitForIdleSync();
+        getInstrumentation().sendStringSync("Jake");*/
 
         // Tap "Greet" button
         // ----------------------
 
         Button greetButton =
-                (Button) activity.findViewById(R.id.greet_button);
+                (Button) this.activity.findViewById(R.id.greet_button);
 
         TouchUtils.clickView(this, greetButton);
 
@@ -48,7 +59,7 @@ public class MainActivityTests extends ActivityInstrumentationTestCase2<MainActi
         // ----------------------
 
         TextView greetMessage =
-                (TextView) activity.findViewById(R.id.message_text_view);
+                (TextView) this.activity.findViewById(R.id.message_text_view);
 
         String actualText = greetMessage.getText().toString();
         String testString = "Hello, Jake!";
